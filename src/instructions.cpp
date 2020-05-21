bae7f3b5cf6a94214c2f733a2c94debabf5d5cbd
r4nx <mod34@yandex.ru>
2020-05-22 02:06:38 +0300

    Add license

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -1,6 +1,22 @@
 /*
- * As it's a huge method, it is taken out into separated file.
+ * granite - CHIP-8 emulator
+ *  Copyright (C) 2020 r4nx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+// As it's a huge method, it is taken out into separated file.
 
 #include "chipvm.hpp"
 
